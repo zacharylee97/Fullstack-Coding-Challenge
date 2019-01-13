b1e927ba1cf5ebc136214096e8bf0216e944d633
Zachary Lee <zacharylhlee@gmail.com>
2019-01-13 12:12:29 -0500

    Creates Websocket server

---
+++ chusj-coding-challenge/server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+// Set the port to 3001
+const PORT = 3001;
+
+// Create a new express server
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// Create the WebSockets server
+const wss = new SocketServer({ server });
+const clients = {};
+
+// Set up a callback that will run when a client connects to the server
+// when a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+  addClient(ws);
+  
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => {
+    wss.broadcastClients();
+    removeClient(ws);
+    console.log('Client disconnected');
+  });
+});
