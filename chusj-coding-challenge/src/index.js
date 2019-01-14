8bd7ad4b8860378c4f89f5d17b7de900c3ebc5a2
Zachary Lee <zacharylhlee@gmail.com>
2019-01-14 10:36:54 -0500

    Edits files to match chat app tutorial

--- chusj-coding-challenge/src/index.js
+++ chusj-coding-challenge/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers'
+import handleNewMessage from './sagas'
+import setupSocket from './sockets'
+import username from './utils/name'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+  );
+
+const socket = setupSocket(store.dispatch, username);
+
+sagaMiddleware.run(handleNewMessage, { socket, username })
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
